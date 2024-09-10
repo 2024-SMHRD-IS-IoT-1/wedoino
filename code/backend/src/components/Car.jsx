8c667e147f3ca3a436608a992af347c51136f70e
yeji <wlfls0305@gmail.com>
2024-09-10 17:53:18 +0900

    Signed-off-by: yeji <wlfls0305@gmail.com>

---
+++ code/backend/src/components/Car.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Car() {
+
+    return (
+        <div>
+            <br />
+            <button style={{height:'60px', width:'30px'}}></button>
+            <span> </span>
+            <button style={{height:'60px', width:'30px'}}></button>
+            <span> </span>
+            <button style={{height:'60px', width:'30px'}}></button>
+            <span> </span>
+            <button style={{height:'60px', width:'30px'}}></button>
+            <span> </span>
+            <button style={{height:'60px', width:'30px'}}></button>
+
+            <br />
+        </div>
+        
+    )
+}
+
+export default Car
