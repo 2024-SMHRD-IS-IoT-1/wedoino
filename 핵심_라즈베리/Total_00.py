455f6913e8489a181b704f4075ef5d2a7fcc68ce
김예지 <113967731+KimYe-Ji0305@users.noreply.github.com>
2024-09-11 09:25:33 +0900

    Update Total_00.py

--- 핵심_라즈베리/Total_00.py
+++ 핵심_라즈베리/Total_00.py
@@ -27,7 +27,7 @@ while(1):
     print(fl)
     system = "n"
     firestation = "n"
-    sql = "insert into tb_flame values (%d,'%s','%s',NOW(),'%s')"%(count,system,firestation,emp_no)
+    sql = "insert into tb_flame values (%d,'%s','%s',NOW(),'%s')"%(count,system,firestation,emp_no,parking_lot)
     DB_0.DB_insert(sql)
     
     #parking 초음파
